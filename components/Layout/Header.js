e3d46285d2d4d8b618e4ff0420515533bac96da6
Abraham Pc <xabr12@gmail.com>
2023-06-20 19:54:44 -0600

    colors

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -51,7 +51,7 @@ const Header = () => {
                   : " text-black-500 hover:text-orange-500 a")
               }
             >
-              Acerca
+              Inicio
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -69,7 +69,7 @@ const Header = () => {
                   : " text-black-500 hover:text-orange-500 ")
               }
             >
-              Carreras
+              Proyectos
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -87,7 +87,7 @@ const Header = () => {
                   : " text-black-500 hover:text-orange-500 ")
               }
             >
-              Proyectos
+              Cursos
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -105,7 +105,7 @@ const Header = () => {
                   : " text-black-500 hover:text-orange-500 ")
               }
             >
-              Profesores
+              Espacios
             </LinkScroll>
           </ul>
           <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
@@ -119,7 +119,6 @@ const Header = () => {
         </nav>
       </header>
       {/* Mobile Navigation */}
-
       <nav className="fixed lg:hidden bottom-0 left-0 right-0 z-20 px-4 sm:px-8 shadow-t ">
         <div className="bg-white-500 sm:px-3">
           <ul className="flex w-full justify-between items-center text-black-500">
@@ -153,7 +152,7 @@ const Header = () => {
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              About
+              Inicio
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -185,7 +184,7 @@ const Header = () => {
                   d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
                 />
               </svg>
-              Feature
+              Proyectos
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -217,7 +216,7 @@ const Header = () => {
                   d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              Pricing
+              Cursos
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -249,7 +248,7 @@ const Header = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 />
               </svg>
-              Testimonial
+              Espacios
             </LinkScroll>
           </ul>
         </div>
